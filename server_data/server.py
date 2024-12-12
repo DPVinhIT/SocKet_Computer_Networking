06a01c8c8dcd257bdbb8345ef3e502bcc977f917
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-12-13 03:09:46 +0700

    Update server.py

--- server_data/server.py
+++ server_data/server.py
@@ -2,7 +2,8 @@ import socket
 import threading
 import os
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import messagebox, ttk, Frame,Text, Button, VERTICAL, WORD, Label
+from tkinter import END
 from openpyxl import Workbook, load_workbook
 import time
 import logging
@@ -23,9 +24,6 @@ FILE_CANCEL_REQUEST = "!CANCEL_DOWNLOAD"
 all_connections = []
 
 #Socket của server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(ADDR)
 
 #File hoạt động của server
 log_directory = "server_data/PRIVATE"
@@ -35,6 +33,7 @@ logging.basicConfig(
     filename=log_file_path,  # Sử dụng đường dẫn đầy đủ
     level=logging.INFO,      # Mức độ log
     format="%(asctime)s - %(levelname)s - %(message)s",  # Định dạng log
+    datefmt='[%d/%m/%Y %H:%M:%S]',
     filemode="a"             # Append mode (ghi thêm vào file)
 )
 
@@ -135,23 +134,16 @@ def receive_message(conn):
     try:
         msg_length = msg_length.decode(FORMAT)
     except Exception as e:
-        print(f"Lỗi: {e}")
+        print(f"Lỗi chiều dài tin nhắn: {e}")
         logging.error(f"Received non-text data, skipping decoding.")
     msg_length = int(msg_length)  # Chuyển đổi chiều dài thông điệp thành số
     msg = conn.recv(msg_length)
     try:
         msg = msg.decode(FORMAT)
     except Exception as e:
-        print(f"Lỗi: {e}")
+        print(f"Lỗi nhận tin nhắn: {e}")
     return msg
     
-def server_listen():
-    while True:
-        conn, addr = server.accept()
-        all_connections.append(conn)
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.daemon =True
-        thread.start()
 ##################################################################################################################################
 # CLIENT DOWNLOAD
 def build_folder_tree(folder_path):
@@ -217,11 +209,10 @@ def handle_download_file(conn,addr,msg):
                         cancel_signal = conn.recv(HEADER).decode(FORMAT).strip()
                         if FILE_CANCEL_REQUEST in cancel_signal:
                             logging.info(f"Download cancelled by client {addr} for file \"{filename}\".")
-                            return
+                            break
                     except socket.timeout:
                         pass  # Không có tín hiệu hủy, tiếp tục gửi
-                
-                    
+                       
             logging.info(f"Download Successful: \"{filename}\" from client {addr}")
         else:
             conn.send("File not found.".encode(FORMAT))
@@ -368,16 +359,19 @@ def handle_data(conn):
 # XỬ LÍ CLIENT
 def handle_client(conn, addr):
     global connected_clients
-    print(f"[NEW CONNECTION] {addr} connected.")
+    chat_box.insert(END,current_time() + f": [NEW CONNECTION] {addr} connected.\n","green")
     
     connected_clients += 1
-    print(f"[ACTIVE CONNECTIONS] {connected_clients} active connections.")
+    chat_box.insert(END,current_time() + f": [ACTIVE CONNECTIONS] {connected_clients} active connections.\n","green")
     logging.info(f"Connect from client {addr}")
 
     logged_in = False  # Trạng thái chưa đăng nhập
 
     try:
         while True:
+                if stop_event.is_set():  # Kiểm tra sự kiện dừng nếu có
+                    break
+                
                 msg = receive_message(conn)
                 
                 if msg == DISCONNECT_MESSAGE:
@@ -433,41 +427,39 @@ def handle_client(conn, addr):
                     else:
                         conn.send("Invalid command.".encode(FORMAT))
     except Exception as e:
-        print(f"Lỗi: {e}")
+        #messagebox.showerror("Lỗi",f"Lỗi handle client: {e}")
         pass
     finally:
         all_connections.remove(conn)
         conn.close()
         connected_clients -= 1
         logging.info(f"!!Disconnect from client {addr}")
-        print(f"[ACTIVE CONNECTIONS] {connected_clients} active connections.")
+        chat_box.insert(END,current_time()+ f": [DISCONNECT] client {addr} disconnect\n","red")
+        chat_box.insert(END,current_time()+ f": [ACTIVE CONNECTIONS] {connected_clients} active connections.\n","green")
 
 ##################################################################################################################################
 # GIAO DIỆN
-def clear_text_box(chat_box):
+def clear_text_box():
     chat_box.config(state="normal")
-    chat_box.delete(1.0, tk.END)
+    chat_box.delete(1.0, END)
     chat_box.config(state="disabled")
     
-def history_log():
-    pass
-
 def current_time():
-    cur_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-    return str(cur_time) + ": "
+    cur_time = time.strftime("%d/%m/%Y %H:%M:%S",time.localtime())
+    return str(cur_time)
 
 def list_all_connecting():
     chat_box.config(state="normal")
     if not all_connections:
-        chat_box.insert(tk.END,current_time()+"Không có kết nối\n")
+        chat_box.insert(END,current_time()+": Không có kết nối\n","red")
     else: 
-        chat_box.insert(tk.END,current_time()+"Tất cả các kết nối là:\n")
+        chat_box.insert(END,current_time()+f": Có {connected_clients} client đang kết nối là:\n","green")
         for conn in all_connections:
             try:
                 addr=conn.getpeername()
-                chat_box.insert(tk.END,f"  Client {addr} đang kết nối\n")
+                chat_box.insert(END,f" -Client {addr} đang kết nối\n","blue")
             except Exception as e:
-                chat_box.insert(tk.END,f"  Lỗi khi lấy thông tin kết nối: {e}\n")
+                chat_box.insert(END,f" -Lỗi khi lấy thông tin kết nối: {e}\n","blue")
     chat_box.config(state="disabled")
   
 def closing_window():
@@ -476,42 +468,123 @@ def closing_window():
             if server.fileno() != -1: 
                 server.close()
         finally:
-            root.destroy()
-    os._exit(0)
+            server_window.quit()
+            server_window.destroy()
+            os._exit(0)
 
-    
-print("[STARTING] Server is starting...")
-server.listen()
-print(f"[LISTENING] Server is listening on {SERVER}")
-logging.info("Server Start!!") #Ghi vào file log
+seconds = 0
+
 # Khởi tạo cửa sổ Tkinter
-global root
-root = tk.Tk()
-root.title("Server Interface")
-root.geometry("700x500")
+global server_window
+server_window = tk.Tk()
+server_window.title("Server Interface")
+server_window.geometry("925x500+300+200")
+server_window.configure(bg="#34495e")
 
-root.protocol("WM_DELETE_WINDOW", closing_window)
-# Xử lý dữ liệu  
-handle_server = threading.Thread(target=server_listen)
-handle_server.daemon = True # Dừng Thread khi đóng giao diện
-handle_server.start()
+server_window.protocol("WM_DELETE_WINDOW", closing_window)
+
+# Xử lý dữ liệu 
+stop_event = threading.Event()
+
+def start_server():
+    global server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(ADDR)
+    
+    chat_box.insert(END,current_time()+": [STARTING] Server is starting...\n","green")
+    server.listen()
+    chat_box.insert(END,current_time()+f": [LISTENING] Server is listening on {SERVER}\n","green")
+    logging.info("[Server Start!!]") #Ghi vào file log
+    
+    show_connecting_button.config(state="normal")
+    end_server_button.config(state="normal")
+    start_server_button.config(state="disabled")
+    
+    # Xử lí đa luồng
+    handle_server = threading.Thread(target=server_listen)
+    handle_server.daemon = True # Dừng Thread khi đóng giao diện
+    handle_server.start()
+    
+def server_listen():
+    while True:
+        conn, addr = server.accept()
+        all_connections.append(conn)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.daemon =True
+        thread.start()
+        
+def end_server():
+    stop_event.set()  # Kích hoạt sự kiện dừng
+    server.close()    # Đóng server
+    chat_box.insert(tk.END, current_time() + ": [STOPPED] Server đã đóng.\n", "red")
+
+    show_connecting_button.config(state="disabled")
+    end_server_button.config(state="disabled")
+    start_server_button.config(state="normal")
 
 # Xử lí giao diện
+# Frame hiện thời gian:
+time_frame = Frame(server_window, bg="#34495e")
+time_frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.05)
+# Frame thông tin
+info_frame = Frame(server_window, bg="#2c3e50", bd=3, relief="solid")
+info_frame.place(relx=0.05, rely=0.1, relwidth=0.9, relheight=0.6)
+# Frame Nút
+button_frame = Frame(server_window, bg="#34495e")
+button_frame.place(relx=0.05, rely=0.72, relwidth=0.9, relheight=0.2)
 
-chat_box = tk.Text(root, bg="light yellow",height = 15,width=75,state="disabled", wrap=tk.WORD)
-chat_box.grid(row=0, column=0, columnspan=10, padx=10, pady=10)
+# Hàm tính thời gian
+def current_datetime():
+    show_current_time.config(text=f"{current_time()}")
+    server_window.after(1000, current_datetime)
+def count_time():
+    global seconds 
+    seconds += 1
+    show_runtime.config(text=f"Runtime: {seconds // 3600:02}:{(seconds % 3600) // 60:02}:{seconds % 60:02}")
+    server_window.after(1000, count_time)
+def show_quantity_connected():
+    show_connected_clients.config(text=f"Số lượng đang kết nối: {connected_clients}")
+    server_window.after(1000, show_quantity_connected)
+# Label thời gian hiển thị
+show_current_time = Label(time_frame, text="", fg="white",bg="#34495e",anchor="w", font=("Arial", 14, "bold"))
+show_current_time.place(relx=0, rely=0, relheight=1, relwidth=0.25)
+show_runtime = Label(time_frame, text="", fg="white",bg="#34495e",anchor="w", font=("Arial", 14, "bold"))
+show_runtime.place(relx=0.25, rely=0, relheight=1, relwidth=0.3)
+show_connected_clients = Label(time_frame,text="", fg="white",bg="#34495e",anchor="w", font=("Arial", 14, "bold"))
+show_connected_clients.place(relx=0.5, rely=0, relheight=1, relwidth=0.3)
 
-scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=chat_box.yview)
-scrollbar.grid(row=0, column=10,sticky="ns")
+current_datetime()
+count_time()
+show_quantity_connected()
+
+# Text box for information
+chat_box = Text(info_frame, bg="#ecf0f1", fg="#2c3e50", font=("Arial", 14), wrap=WORD)
+chat_box.place(relx=0, relheight=1, relwidth=1)
+chat_box.tag_configure("red", foreground="red")
+chat_box.tag_configure("green", foreground="green")
+chat_box.tag_configure("blue", foreground="blue")
+chat_box.tag_configure("black",foreground="black")
+
+# Scrollbar for text box
+scrollbar = ttk.Scrollbar(info_frame, orient=VERTICAL, command=chat_box.yview)
+scrollbar.place(relx=0.98, rely=0.004, relheight=0.99, relwidth=0.02)
 chat_box.config(yscrollcommand=scrollbar.set)
 
-button_clear_text_box = tk.Button(root, text="Clear all text", command=lambda: clear_text_box(chat_box))
-button_clear_text_box.grid(row=2, column=0, padx=10, pady=10)
+start_server_button = Button(button_frame, text="Start Server",command=start_server
+                             , bg="#27ae60", fg="white", font=("Helvetica", 16, "bold"))
+start_server_button.place(relx=0, rely=0.3, relwidth=0.22, relheight=0.6)
 
-button_all_connected = tk.Button(root, text="ALL connected", command=list_all_connecting)
-button_all_connected.grid(row=3, column=0, padx=10, pady=10)
+end_server_button = Button(button_frame, text="End Server",command=end_server
+                           , bg="#c0392b", fg="white", font=("Helvetica", 16, "bold"),state="disabled")
+end_server_button.place(relx=0.25, rely=0.3, relwidth=0.22, relheight=0.6)
 
-button_history_log = tk.Button(root, text="History log", command=history_log)
-button_history_log.grid(row=4, column=0, padx=10, pady=10)
+show_connecting_button = Button(button_frame, text="Show All Connecting", command=list_all_connecting
+                                , bg="#2980b9", fg="white", font=("Helvetica", 16, "bold"),state="disabled")
+show_connecting_button.place(relx=0.51, rely=0.3, relwidth=0.28, relheight=0.6)
 
-root.mainloop() 
+clear_text_button = Button(button_frame, text="Clear text", command=clear_text_box
+                           , bg="#f1c40f", fg="white", font=("Helvetica", 16, "bold"))
+clear_text_button.place(relx=0.82, rely=0.3, relwidth=0.18, relheight=0.6)
+
+server_window.mainloop() 
